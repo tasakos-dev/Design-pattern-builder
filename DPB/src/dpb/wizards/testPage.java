290408fc3a9c45c4e3c34a8a754e115f29807c97
tasakos-dev <tassosliot@protonmail.com>
2022-10-23 02:41:20 +0300

    Add dropdown lists on first page

--- DPB/src/dpb/wizards/testPage.java
+++ DPB/src/dpb/wizards/testPage.java
@@ -3,13 +3,18 @@ package dpb.wizards;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.swt.widgets.Button;
+
 
 public class testPage extends WizardPage {
 	private ISelection selection;
+	
+	
 
 	public testPage(ISelection selection) {
 		super("wizardPage");
@@ -21,17 +26,36 @@ public class testPage extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
+		
+		String[] strings = new String[] {"hello", "hi"};
 
 		setControl(container);
 		
-		Button clickHello = new Button(container, SWT.NONE);
-		clickHello.addSelectionListener(new SelectionAdapter() {
+		ComboViewer comboViewer = new ComboViewer(container, SWT.BORDER | SWT.READ_ONLY);
+		Combo combo = comboViewer.getCombo();
+		
+		combo.setBounds(26, 77, 201, 38);
+		combo.setItems(strings);
+		
+		ComboViewer comboViewer_1 = new ComboViewer(container, SWT.READ_ONLY);
+		Combo combo_1 = comboViewer_1.getCombo();
+		
+		combo.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				System.out.println("hello");
+				System.err.println("hhoeoeoeo");
+				if (combo.getSelection().equals("hello"))
+					combo_1.setItems(new String[] {"hello", "hi1"});
+				else
+					combo_1.setItems(new String[] {"hello", "hi2"});
 			}
 		});
-		clickHello.setBounds(249, 96, 83, 32);
-		clickHello.setText("New Button");
+		
+
+		
+		combo_1.setBounds(301, 77, 201, 38);
+		
+	
+		
 	}
 }
