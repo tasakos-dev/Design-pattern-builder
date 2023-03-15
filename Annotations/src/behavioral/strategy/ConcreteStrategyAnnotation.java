562d4cce9f278f152a4ff38a04a2f97eb1868f57
tasakos-dev <tassosliot@protonmail.com>
2023-03-15 20:10:53 +0200

    Finish with annotations

---
+++ Annotations/src/behavioral/strategy/ConcreteStrategyAnnotation.java
@@ -0,0 +1,13 @@
+package behavioral.strategy;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface ConcreteStrategyAnnotation {
+
+}
